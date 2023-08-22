ec71416d165bf799ab200a9d9ab8ed75d9bc7ff5
mengyu <1149095383@qq.com>
2023-08-22 16:50:17 +0800

    【8.22】构建了一部分主页面的内容，可以实现输入index进行跳转

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <router-link to="/hello"> Hello </router-link>
-  <router-view />
-  <router-view />
+  <div class = "jump">
+    <el-form-item :inline="true">
+      <div class="jump-input">
+        <el-input type="success" :placeholder="placeholderInfo" v-model="jumpDestination" clearable>   
+        </el-input>
+      </div>
+      <div class="jump-button">
+        <router-link :to="'/'+jumpDestination">
+          <el-button type="success">Jump</el-button>
+        </router-link>
+      </div>
+    </el-form-item>
+  </div>
+
+  <div class="show">
+    <router-view />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Home from './components/Home.vue'
+import Error from './components/Error.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Home
+    Home,
+    Error
+  },
+  data(){
+    return {
+      placeholderInfo: "Please input the jump destination here.",
+      countNumber: 0,
+      jumpDestination: ''
+    }
   },
   methods:{
-    ShowInfo(){
-      alert(this.$route)
+    // 这里可以写自己的函数，调用的时候可以使用vue的绑定的方式进行响应，例如下：
+    /*
+      yourFunction(){
+        console.log("*")
+      }
+    */
+    onCounting(){
+      this.countNumber++
     }
+  },
+    // created是一个生命周期函数，里面的内容会在vue页面创建完成之后进行加载，这里我调用了onCounting
+    // 函数，对data中的countNumber进行自增，然后输出到控制台中(F12)
+  created(){
+    this.onCounting()
+    console.log(this.countNumber)
   }
 }
 </script>
@@ -32,4 +66,20 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.jump {
+  display: inline-block;
+}
+
+.jump-input {
+  width: 270px;
+}
+
+.jump-button{
+  margin-left: 10px;
+}
+.show {
+  margin-top: 50px;
+}
+
 </style>
